0331126505bcac5a826a1826b94cb5897a6a900a
Timofey <timadol@yandex.ru>
2019-03-18 23:30:58 +0300

    Вроде работает диффузия, но не очень работает адвекция, нужно копаться дальше

--- Euler.js
+++ Euler.js
@@ -1,15 +1,13 @@
 /* eslint-disable no-undef */
-const DURATION = 10000;
 const N = 100;
-const GRID_SIZE = 4;
-
-
+const CELL_SIZE = 4;
+const TIME_MULTIPLIER = 1;
 
 let gridWidth = N;
 let gridHeight = N;
-let width = GRID_SIZE * gridWidth;
-let height = GRID_SIZE * gridHeight;
-let diff = 1;
+let width = CELL_SIZE * gridWidth;
+let height = CELL_SIZE * gridHeight;
+let diff = 10;
 
 var fieldGrid;
 
@@ -31,145 +29,161 @@ function start(ctx) {
   function animationLoop() {
     let time = new Date();
     time -= start;
-    densStep(time);
-    drawDens(ctx, GRID_SIZE);
-    // test
-
-    // ctx.fillSyle = "#000000";
-    // ctx.fillRect(time/10,time/10,50,50);
-
+    time /= TIME_MULTIPLIER;
     start = new Date();
+    //diffuse(fieldGrid.v, time, 2);
+    //diffuse(fieldGrid.u, time, 1);
+    //advect(fieldGrid.v, fieldGrid.u, fieldGrid.v, time, 2);
+    //advect(fieldGrid.u, fieldGrid.u, fieldGrid.v, time, 1);
+
+    diffuse(fieldGrid.t, time,0);
+    advect(fieldGrid.t, fieldGrid.u, fieldGrid.v, time, 0);
+    drawField(ctx, CELL_SIZE);
+    //alert();
     requestAnimationFrame(animationLoop);
   }
   var start = new Date();
   requestAnimationFrame(animationLoop);
 }
 
-function drawDens(ctx, multiplier) {
+function drawField(ctx, multiplier) {
   ctx.clearRect(0, 0, width, height);
   for (let i = 0; i < gridHeight; i++) {
     for (let j = 0; j < gridWidth; j++) {
-      let temp = fieldGrid.dens[i * gridWidth + j];
-      ctx.fillStyle = rgbToHex(temp / 2 * 255, temp / 2 * 255, temp / 2 * 255);
-      ctx.fillRect(i * multiplier, j * multiplier, multiplier, multiplier);
+      let x = fieldGrid.t[i * gridWidth + j];
+      x = x / 200;
+      ctx.fillStyle = rgbToHex(x * 255, (1 - x) * 255, (1 - x) * 255);
+      ctx.fillRect(j * multiplier, i * multiplier, multiplier, multiplier);
     }
   }
+}
 
+function coor(x, y) {
+  return gridHeight * y + x;
+}
+
+function diffuse(x, dt,type) {
+  let a = dt * diff;
+  x0 = x.concat([]);
+  for (let k = 0; k < 20; k++) {
+    for (let i = 1; i < gridHeight - 1; i++) {
+      for (let j = 1; j < gridWidth - 1; j++) {
+        x[coor(j, i)] =
+          (x0[coor(j, i)] +
+            a *
+              (x0[coor(j + 1, i)] +
+                x0[coor(j - 1, i)] +
+                x0[coor(j, i - 1)] +
+                x0[coor(j, i + 1)])) /
+          (1 + 4 * a);
+      }
+    }
+    setBnd(x,type);
+  }
+}
+
+function advect(d, u, v, dt, type) {
+  let t, b, l, r;
+  let x, y;
+  var d0 = d.concat([]);
+  for (i = 1; i < gridWidth - 1; i++) {
+    for (j = 1; j < gridHeight - 1; j++) {
+      x = i - dt * v[coor(i, j)];
+      y = j - dt * u[coor(i, j)];
+      if (x < 0.5) x = 0.5;
+      if (x > gridWidth + 0.5) x = gridWidth + 0.5;
+      if (y < 0.5) y = 0.5;
+      if (y > gridHeight + 0.5) y = gridHeight + 0.5;
+      l = x % 1;
+      r = 1 - l;
+      t = y % 1;
+      b = 1 - t;
+      let dbl = d0[coor(Math.floor(x), Math.floor(y))];
+      let dtl = d0[coor(Math.floor(x), Math.floor(y + 1))];
+      let dbr = d0[coor(Math.floor(x + 1), Math.floor(y))];
+      let dtr = d0[coor(Math.floor(x + 1), Math.floor(y + 1))];
+      d[coor(i, j)] = t * (l * dtl + r * dtr) + b * (l * dbl + r * dbr);
+    }
+  }
+  setBnd(d, type);
+}
+
+function addTsource() {}
+
+function setBnd(x, type) {
+  if (type == 0) { //Граничные условия для температуры
+    for (let i = 0; i < gridWidth; i++) {
+      x[i] = x[gridWidth + i];
+    }
+    for (let i = 0; i < gridWidth; i++) {
+      x[gridWidth * (gridHeight - 1) + i] =
+        x[gridWidth * (gridHeight - 1) + i - 1];
+    }
+    for (let i = 1; i < gridHeight; i++) {
+      x[i * gridWidth] = x[i * gridWidth + 1];
+    }
+    for (let i = 1; i < gridHeight; i++) {
+      x[i * gridWidth + gridWidth - 1] = x[i * gridWidth + gridWidth - 2];
+    }
+  }
+  if (type == 1){//Граничные условия для y компоненты скорости
+    for (let i = 0; i < gridWidth; i++) {
+      x[i] = Math.abs(x[gridWidth + i]);
+    }
+    for (let i = 0; i < gridWidth; i++) {
+      x[gridWidth * (gridHeight - 1) + i] =
+        +Math.abs(x[gridWidth * (gridHeight - 1) + i - 1]);
+    }
+    for (let i = 1; i < gridHeight; i++) {
+      x[i * gridWidth] = x[i * gridWidth + 1];
+    }
+    for (let i = 1; i < gridHeight; i++) {
+      x[i * gridWidth + gridWidth - 1] = x[i * gridWidth + gridWidth - 2];
+    }
+  }
+  if (type == 2){//Граничные условия для x компоненты скорости
+    for (let i = 0; i < gridWidth; i++) {
+      x[i] = x[gridWidth + i];
+    }
+    for (let i = 0; i < gridWidth; i++) {
+      x[gridWidth * (gridHeight - 1) + i] =
+      x[gridWidth * (gridHeight - 1) + i - 1];
+    }
+    for (let i = 1; i < gridHeight; i++) {
+      x[i * gridWidth] = Math.abs(x[gridWidth * (gridHeight - 1) + i - 1]);
+    }
+    for (let i = 1; i < gridHeight; i++) {
+      x[i * gridWidth + gridWidth - 1] = -Math.abs(x[i * gridWidth + gridWidth - 2]);
+    }
+  }
 }
 
 function gridInitialize(fieldGrid) {
   for (let i = 0; i < gridHeight; i++) {
     for (let j = 0; j < gridWidth; j++) {
+      if (i > 40 && i < 60) fieldGrid.t.push(200);
+      else fieldGrid.t.push(0);
+      fieldGrid.prevT.push(0);
+      // fieldGrid.prevV.push(0);
+      // fieldGrid.prevU.push(0);
       fieldGrid.v.push(0);
-      fieldGrid.prevV.push(0);
+      if ((j > 40 && j < 60)&&(i > 45 && i < 55))  fieldGrid.u.push(0.01);
+      else  
       fieldGrid.u.push(0);
-      fieldGrid.prevU.push((gridHeight / 2 - i)/gridHeight);
-      fieldGrid.dens.push(1);
-      fieldGrid.prevDens.push(0);
     }
   }
 }
-
-function diffuse(x, x0, dt) {
-  let a = dt * diff * gridHeight * gridWidth;
-  for (let k = 0; k < 20; k++) {
-    for (let i = 1; i < gridHeight - 1; i++) {
-      for (let j = 1; j < gridWidth - 1; j++) {
-        x[i * gridWidth + j] =
-          (x0[i * gridWidth + j] +
-            a *
-            (x[(i - 1) * gridWidth + j] +
-              x[(i + 1) * gridWidth + j] +
-              x[i * gridWidth + j + 1] +
-              x[i * gridWidth + j - 1])) /
-          (1 + 4 * a);
-      }
-    }
-    setBnd(0, x);
-  }
-  
-}
-
-function advect(d, d0, v, u, dt, b) {
-  let i0, i1, j0, j1;
-  let x, y, s0, t0, s1, t1;
-  let dt0 = dt * N;
-  for (let i = 1; i < gridHeight - 1; i++) {
-    for (let j = 1; j < gridWidth - 1; j++) {
-      x = i - dt0 * u[i * gridWidth + j];
-      y = i - dt0 * v[i * gridWidth + j];
-      if (x < 0.5) x = 0.5;
-      if (x > gridWidth + 0.5) x = gridWidth + 0.5;
-      if (y < 0.5) x = 0.5;
-      if (y > gridWidth + 0.5) x = gridHeight + 0.5;
-      i0 = Math.floor(x);
-      i1 = i0 + 1;
-      j0 = Math.floor(y);
-      j1 = j0 + 1;
-      s1 = x - i0;
-      s0 = 1 - s1;
-      t1 = y - j0;
-      t0 = 1 - t1;
-      d[i * gridWidth + j] = s0 * (t0 * d0[i0 * gridWidth + j0] + t1 * d0[i0 * gridWidth + j1]) +
-        s1 * (t0 * d0[i1 * gridWidth + j0] + t1 * d0[i1 * gridWidth + j1]);
-    }
-  }
-  setBnd(b, d);
-}
-
-function setBnd(bndType, x) {
-  for (let i = 1; i < gridWidth - 1; i++) {
-    x[0 * gridWidth + i] = x[1 * gridWidth + i];
-    x[(gridHeight - 1) * gridWidth + i] = x[(gridHeight - 2) * gridWidth + i];
-  }
-  for (let i = 1; i < gridHeight - 1; i++) {
-    x[i * gridWidth + 0] = x[i * gridWidth + 1];
-    x[i * gridWidth + gridWidth - 1] = x[i * gridWidth + gridWidth - 2];
-  }
-  x[0 * gridWidth + 0] = (x[1 * gridWidth + 0] + x[0 * gridWidth + 1])/2;
-  x[0 * gridWidth + gridWidth - 1] = (x[1 * gridWidth + gridWidth - 1] + x[0 * gridWidth + gridWidth - 2])/2;
-  x[(gridHeight - 1) * gridWidth + 0] = (x[(gridHeight - 2) * gridWidth + 0] + x[(gridHeight - 1) * gridWidth + 1])/2;
-  x[(gridHeight - 1) * gridWidth + gridWidth - 1] = (x[(gridHeight - 2) * gridWidth + gridWidth - 1] + x[(gridHeight - 1) * gridWidth + gridWidth - 2])/2;
-}
-
-function addSource(x, x0, dt) {
-  var temp = x;
-  for (let i = 0; i < gridHeight * gridWidth; i++) {
-    x[i] += x0[i] * dt;
-
-  }
-  x0 = temp;
-}
-
-function swap(x,y){
-  let temp = x;
-  x=y;
-  y=temp;
-}
-
-function densStep(dt) {
-  addSource(fieldGrid.dens, fieldGrid.prevDens, dt);
-  diffuse(fieldGrid.dens, fieldGrid.prevDens, dt);
-  advect( fieldGrid.dens,fieldGrid.prevDens, fieldGrid.v, fieldGrid.u, dt);
-}
-
-(function () {
-  const requestAnimationFrame =
-    window.requestAnimationFrame ||
-    window.mozRequestAnimationFrame ||
-    window.webkitRequestAnimationFrame ||
-    window.msRequestAnimationFrame;
-  window.requestAnimationFrame = requestAnimationFrame;
-})();
 
 function init() {
   fieldGrid = {
+    t: [],
+    prevT: [],
+    tSource: [],
     v: [],
-    prevV: [],
-    u: [],
-    prevU: [],
-    dens: [],
-    prevDens: []
+    // prevV: [],
+    u: []
+    // prevU: [],
+    //display: []
   };
   gridInitialize(fieldGrid);
   canvasEl = document.getElementById("el");

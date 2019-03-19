cc8c6dee1515d1773e906379179bff516b8340e8
Timofey <timadol@yandex.ru>
2019-03-19 15:10:34 +0300

    Вроде адвекция работает, но нужно тестить

--- Euler.js
+++ Euler.js
@@ -1,13 +1,13 @@
 /* eslint-disable no-undef */
 const N = 100;
 const CELL_SIZE = 4;
-const TIME_MULTIPLIER = 1;
+const TIME_MULTIPLIER = 1000;
 
 let gridWidth = N;
 let gridHeight = N;
 let width = CELL_SIZE * gridWidth;
 let height = CELL_SIZE * gridHeight;
-let diff = 1;
+let diff = 100;
 
 var fieldGrid;
 
@@ -53,9 +53,9 @@ function start(ctx) {
     time -= start;
     time /= TIME_MULTIPLIER;
     start = new Date();
-
+    
     diffuse(fieldGrid.t, time, 0);
-    //advect(fieldGrid.t,fieldGrid.v,fieldGrid.u,time,0);
+    advect(fieldGrid.t,fieldGrid.v,fieldGrid.u,time,0);
     drawField(ctx, CELL_SIZE);
     requestAnimationFrame(animationLoop);
   }
@@ -67,16 +67,16 @@ function drawField(ctx, multiplier) {
   ctx.clearRect(0, 0, width, height);
   for (let i = 0; i < gridHeight; i++) {
     for (let j = 0; j < gridWidth; j++) {
-      let x = fieldGrid.t[i * gridWidth + j];
-      x = x / 200;
-      ctx.fillStyle = rgbToHex(hToRgb((1 - x) * 240));
+      let display = fieldGrid.t[i * gridWidth + j];
+      display = display / 200;
+      ctx.fillStyle = rgbToHex(hToRgb((1 - display) * 240));
       ctx.fillRect(j * multiplier, i * multiplier, multiplier, multiplier);
     }
   }
 }
 
 function coor(x, y) {
-  return gridHeight * y + x;
+  return gridWidth * y + x;
 }
 
 function diffuse(d, dt, type) {
@@ -100,40 +100,47 @@ function diffuse(d, dt, type) {
   }
 }
 
-function advect(d, v, u, dt, type) {
-  let d0 = d.concat([]);
-  for (let i = 1; i < gridHeight - 1; i++) {
-    for (let j = 1; j < gridWidth - 1; j++) {
-      let y = i - dt * u[coor(j, i)];
-      let x = j - dt * v[coor(j, i)];
-      if (x<0.5) x = 0.5;
-      if (x>gridWidth-0.5) x = gridWidth-0.5;
-      if (y<0.5) y = 0.5;
-      if (y>gridHeight - 0.5) x = gridHeight -0.5;
-      let kTL = (y % 1) * (x % 1);
-      let kTR = (y % 1) * (1 - (x % 1));
-      let kBL = (1 - (y % 1)) * (x % 1);
-      let kBR = (1 - (y % 1)) * (1 - (x % 1));
-      let tL = d0[coor(Math.floor(x)-1, Math.floor(y))-1];
-      let tR = d0[coor(Math.floor(x), Math.floor(y))-1];
-      let bL = d0[coor(Math.floor(x)-1, Math.floor(y))];
-      let bR = d0[coor(Math.floor(x), Math.floor(y))];
-      d[coor(j, i)] = kTL * tL + kTR * tR * kBL * bL * kBR * bR;
+function advect(d, u, v, dt, type) {
+  let t, b, l, r;
+  let x, y;
+  var d0 = d.concat([]);
+  for (i = 1; i < gridWidth - 1; i++) {
+    for (j = 1; j < gridHeight - 1; j++) {
+      x = i - dt * u[coor(i, j)];
+      y = j - dt * v[coor(i, j)];
+      if (x < 1) x = 1;
+      if (x > gridWidth - 1) x = gridWidth - 1;
+      if (y < 1) y = 1;
+      if (y > gridHeight - 1) y = gridHeight - 1;
+      l = x % 1;
+      r = 1 - l;
+      t = y % 1;
+      b = 1 - t;
+      let dbl = d0[coor(Math.floor(x)-1, Math.floor(y))];
+      let dtl = d0[coor(Math.floor(x)-1, Math.floor(y)-1)];
+      let dbr = d0[coor(Math.floor(x), Math.floor(y))];
+      let dtr = d0[coor(Math.floor(x), Math.floor(y)-1)];
+      d[coor(i, j)] = t * (l * dtl + r * dtr) + b * (l * dbl + r * dbr);
+      if (dbl > 100){
+        let test = 0;
+      }
     }
   }
-  setBnd(d,type);
+  setBnd(d, type);
 }
 
 function setBnd(x, type) {
   if (type == 0) {
     //Граничные условия для температуры
     for (let i = 0; i < gridWidth; i++) {
-      if (i > 40 && i < 60) x[i] = x[gridWidth + i];
-      else x[i] = 200;
+      if (i > 20 && i < 80) 
+      x[i] = x[gridWidth + i];
+      else 
+      x[i] = 200;
     }
     for (let i = 0; i < gridWidth; i++) {
-      //x[gridWidth * (gridHeight - 1) + i] = x[gridWidth * (gridHeight - 1) + i - 1];
-      x[gridWidth * (gridHeight - 1) + i] = 0;
+      x[gridWidth * (gridHeight - 1) + i] = x[gridWidth * (gridHeight - 1) + i - 1];
+      //x[gridWidth * (gridHeight - 1) + i] = 0;
     }
     for (let i = 1; i < gridHeight; i++) {
       x[i * gridWidth] = x[i * gridWidth + 1];
@@ -182,14 +189,12 @@ function setBnd(x, type) {
 function gridInitialize(fieldGrid) {
   for (let i = 0; i < gridHeight; i++) {
     for (let j = 0; j < gridWidth; j++) {
-      if (i > 40 && i < 60) fieldGrid.t.push(200);
+      if ((j > 40 && j < 60) && (i > 45 && i < 55)) fieldGrid.t.push(200);
       else fieldGrid.t.push(0);
       fieldGrid.prevT.push(0);
       // fieldGrid.prevV.push(0);
       // fieldGrid.prevU.push(0);
       fieldGrid.v.push(0);
-      //if (j > 40 && j < 60 && (i > 45 && i < 55)) fieldGrid.u.push(0.01);
-      //else 
       fieldGrid.u.push(0);
     }
   }
